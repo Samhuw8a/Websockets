1c6159c65b63b68640872404d440c8fa427d251d
Samhuw8a <77882404+Samhuw8a@users.noreply.github.com>
2022-10-09 14:19:08 +0200

    Updated Client

--- client.py
+++ client.py
@@ -1,37 +1,16 @@
 import asyncio
 import websockets
 
-class Client():
-    def __init__(self,uri:str)->None:
-        self.uri  = uri
-
-    async def send(self, info:str)->None:
-        async with websockets.connect(self.uri) as websocket:
-            await websocket.send(info)
-            #  print(f"sent :{info}")
-            resp:str = await websocket.recv()
-            #  print(f"got: {resp}")
-            print(f"< {resp}")
-
-def main()->None:
-    client=Client("")
-    while True:
-        inp=input("> ")
-        #  client.send(inp)
-        asyncio.get_event_loop().run_until_complete(client.send(inp))
-
-if __name__ == "__main__":
-    main()
-
-
-async def hello():
+async def hello(info):
     uri = "ws://jonas-server.internet-box.ch:7070"
     async with websockets.connect(uri) as websocket:
-        info ="deez"
         await websocket.send(info)
-        print(f"sent: {info}")
+        #  print(f"sent: {info}")
 
         response = await websocket.recv()
-        print(f"got {response}")
+        #  print(f"got {response}")
 
-#  asyncio.get_event_loop().run_until_complete(hello())
+
+while True:
+    inp=input("> ")
+    asyncio.get_event_loop().run_until_complete(hello(inp))
